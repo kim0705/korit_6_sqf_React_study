918d38a123f55b6cba44f42863bbbb06c42d09b8
kim0705 <dbwls138@naver.com>
2024-06-24 17:44:43 +0900

    6/24 react 수업

--- first_project/src/ch11/components/DataTable/DataTable.jsx
+++ first_project/src/ch11/components/DataTable/DataTable.jsx
@@ -1,17 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataTableBody from "../DataTableBody/DataTableBody";
 import DataTableHeader from "../DataTableHeader/DataTableHeader";
 import "./style.css";
 import { SAMPLE_PRODUCTS } from "../../constants/sampleProducts";
 
+
 function DataTable() {
+    const [isLoad, setLoad] = useState(false);
     const [mode, setMode] = useState(0); // 0이면 조회, 1이면 추가, 2면 수정, 3이면 삭제
     const [products, setProducts] = useState([...SAMPLE_PRODUCTS]);
+    const [isDeleting, setDeleting] = useState(false);
+
+    useEffect(() => {
+        const lsProducts = localStorage.getItem("products");
+        setProducts(!lsProducts ? [] : JSON.parse(lsProducts));
+        setLoad(true);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("products", JSON.stringify(products));
+    }, [products])
 
     return (
         <div className="table-main-container">
-            <DataTableHeader mode={mode} setMode={setMode} />
-            <DataTableBody mode={mode} products={products} />
+            <DataTableHeader mode={mode} setMode={setMode} setProducts={setProducts} setDeleting={setDeleting}/>
+            <DataTableBody mode={mode} setMode={setMode} products={products} setProducts={setProducts} isDeleting={isDeleting} setDeleting={setDeleting}/>
         </div>
     );
 }
--- first_project/src/ch11/components/DataTableBody/DataTableBody.jsx
+++ first_project/src/ch11/components/DataTableBody/DataTableBody.jsx
@@ -1,12 +1,100 @@
+import { useEffect, useRef, useState } from "react";
 import "./style.css";
 
-function DataTableBody({ mode, products }) {
+function DataTableBody({ mode, setMode,  products, setProducts, isDeleting, setDeleting }) {
+    const [viewProducts, setViewProducts] = useState([]);
+    const [checkedAll, setCheckedAll] = useState(false);
+
+    useEffect(() => {
+        if(mode === 0) {
+            resetViewProducts();
+            setCheckedAll(false);
+        }
+    }, [products, mode]);
+
+    useEffect(() => {
+        const checkStates = viewProducts.map(product => product.isChecked);  // product의 isChecked 속성만 배열로 만들어서 checkStates에 저장
+        if (checkStates.includes(false)) {
+            setCheckedAll(false);
+        } else {
+            setCheckedAll(true);
+        }
+    }, [viewProducts]);
+
+    useEffect(() => {
+        if (isDeleting) {
+            setProducts([...viewProducts
+                .filter(viewProduct => viewProduct.isChecked === false)
+                .map(viewProduct => {
+                    const { isChecked, ...product } = viewProduct;
+                    return product;
+                })
+            ]);
+            setMode(0);
+            setDeleting(false);
+        }
+    }, [isDeleting]);
+
+    const resetViewProducts = () => {
+        setViewProducts([...products.map(product => ({ ...product, isChecked: false }))]); // 화살표 뒤에 ()는 값, {}는 함수
+    }
+
+    const handleCheckedAllChange = (e) => {
+        setCheckedAll(checked => {
+            if (!checked) {
+                setViewProducts([...products.map(product => ({ ...product, isChecked: true }))]);
+            } else {
+                resetViewProducts();
+            }
+            return !checked;
+        });
+    }
+
+    const handleCheckedChange = (e) => {
+        if (mode === 2) {
+            setViewProducts(viewProducts => {
+                return [...viewProducts.map(product => {
+                    if (product.id === parseInt(e.target.value)) {
+                        return {
+                            ...product,
+                            isChecked: !product.isChecked
+                        }
+                    }
+                    return {
+                        ...product,
+                        isChecked: false
+                    }
+                })]
+            });
+        }
+        if (mode === 3) {
+            setViewProducts(viewProducts => {
+                return [...viewProducts.map(product => {
+                    if (product.id === parseInt(e.target.value)) {
+                        return {
+                            ...product,
+                            isChecked: !product.isChecked
+                        }
+                    }
+                    return product;
+                })]
+            });
+        }
+    }
+
     return (
         <div className="table-body">
             <table>
                 <thead>
                     <tr>
-                        <th><input type="checkbox" disabled={mode !== 3} /></th>
+                        <th>
+                            <input
+                                type="checkbox"
+                                disabled={mode !== 3}
+                                onChange={handleCheckedAllChange}
+                                checked={checkedAll}
+                            />
+                        </th>
                         <th>상품코드</th>
                         <th>상품명</th>
                         <th>사이즈</th>
@@ -16,9 +104,16 @@ function DataTableBody({ mode, products }) {
                 </thead>
                 <tbody>
                     {
-                        products.map(product => (
+                        viewProducts.map(product => (
                             <tr key={product.id}>
-                                <th><input type="checkbox" disabled={mode === 0 || mode === 1} /></th>
+                                <th>
+                                    <input
+                                        type="checkbox"
+                                        disabled={mode === 0 || mode === 1}
+                                        checked={product.isChecked}
+                                        onChange={handleCheckedChange}
+                                        value={product.id}
+                                    /></th>
                                 <td>{product.id}</td>
                                 <td>{product.productName}</td>
                                 <td>{product.size}</td>
@@ -27,8 +122,6 @@ function DataTableBody({ mode, products }) {
                             </tr>
                         ))
                     }
-
-
                 </tbody>
             </table>
         </div>
